1605eb757890cca2e1a1c1175d5246e0a285e330
ASLIMICHJOSE <aslimich22@example.com>
2025-09-15 20:27:33 +0530

    Deleted all files except README.md

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-btn').addEventListener('click', function () {
-  this.classList.toggle('active');   // toggles hamburger animation
-  document.getElementById('menu').classList.toggle('active'); // toggles menu
-});
